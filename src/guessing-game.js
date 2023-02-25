1dbfc75c80853de7040c95cd9d3551b0b04259d0
Prigozhaya Liaz <elizavetaprigozhaya@gmail.com>
2023-02-25 14:40:46 +0300

    feat: add guessing game class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,29 @@
 class GuessingGame {
     constructor() {}
-
+    arrayOfGuessing = [];
+    indexLeft = undefined;
+    indexRight = undefined;
+    indexMiddle = undefined;
+  
     setRange(min, max) {
-
+        this.indexLeft = min;
+        this.indexRight = max;
+      
     }
-
+  
     guess() {
-
+      this.indexMiddle = Math.ceil((this.indexLeft + this.indexRight) / 2);
+      return this.indexMiddle;
     }
-
+  
     lower() {
-
+      this.indexRight = this.indexMiddle;
     }
-
+  
     greater() {
-
+        this.indexLeft = this.indexMiddle;
     }
-}
+  }
 
 module.exports = GuessingGame;
+
